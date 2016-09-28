c5a4935ebef2d2c6ea4a1d7247fad1416a14658a
Spasimira Hadzhieva <spasimira.d.hadzhieva@gmail.com>
2016-09-28 22:28:30 +0300

    some-templates

---
+++ Working-project/js/script.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    $(document).ready(function() {
+        setActiveClass();
+    });
+
+    function setActiveClass() {
+        $('.navbar-nav.navbar-right li').each(function() {
+            var item = $(this);
+            item.on('click', function() {
+                var item = $(this);
+                item.addClass('active');
+                
+
+            })
+});
+    }
+
+})();
