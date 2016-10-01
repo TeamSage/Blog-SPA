6efd091ef91a2a3d3b7c26a5fb21d8590bcec852
Todor Georgiev <todorgeorgiev96@gmail.com>
2016-10-01 17:04:42 +0300

    logic changes

--- Working-project/scripts/requester.js
+++ Working-project/scripts/requester.js
@@ -1,78 +1,124 @@
 /*jshint esversion: 6 */
+import 'jquery';
 
-let requester = {
-    get(url) {
-        let promise = new Promise((resolve, reject) => {
-            $.ajax({
-                url,
-                method: 'GET',
-                success(response) {
-                    resolve(response);
-                },
-                error(err) {
-                    reject(err);
-                }
-            });
+function send(method, url, options) { 
+    options = options || {};
+
+    var headers = options.headers || {},
+        data = options.data || undefined;
+
+    var promise = new Promise(function (resolve, reject) {
+        $.ajax({
+            url: url,
+            method: method,
+            contentType: 'application/json',
+            headers: headers,
+            data: JSON.stringify(data),
+            success: function (res) {
+                resolve(res);
+            },
+            error: function (err) {
+                reject(err);
+            }
         });
-        return promise;
-    },
-    putJSON(url, body, options = {}) {
-    	let headers = options.headers || {};
-        let promise = new Promise((resolve, reject) => {
-            $.ajax({
-                url,
-                method: 'PUT',
-                contentType: 'application/json',
-                data: JSON.stringify(body),
-                headers,
-                success(data) {
-                    resolve(data);
-                },
-                error(err) {
-                    reject(JSON.parse(err.responseText));
-                }
-            });
-        });
-        return promise;
-    },
-    postJSON(url, body, options = {}) {
-    	let headers = options.headers || {};
-    	let promise = new Promise((resolve, reject) => {
-    		$.ajax({
-    			url,
-    			method: 'POST',
-    			contentType: 'application/json',
-    			data: JSON.stringify(body),
-    			headers,
-                success(data) {
-                    resolve(data);
-                },
-                error(err) {
-                    reject(JSON.parse(err.responseText));
-                }
-    		});
-    	});
-    	return promise;
-    },
-    getJSON(url, options = {}) {
-    	let headers = options.headers || {};
-    	let promise = new Promise((resolve, reject) => {
-    		$.ajax({
-    			url,
-    			method: 'GET',
-    			headers,
-                success(response) {
-                    resolve(response);
-                },
-                error(err) {
-                    reject(JSON.parse(err.responseText));
-                }
-    		});
-    	});
+    });
+    return promise;
+ }
 
-        return promise;
-    }
+ class Requester {
+     get(url, options) {
+         return send('GET', url, options);
+     }
 
-};
+     post(url, options) {
+         return send('POST', url, options);
+     }
 
-export { requester };
+     put(url, options) {
+         return send('PUT', url, options);
+     }
+ }
+
+const requester = new Requester();
+
+export {requester};
+
+// 
+// 
+// let requester = {
+    // get(url) {
+        // let promise = new Promise((resolve, reject) => {
+            // $.ajax({
+                // url,
+                // method: 'GET',
+                // success(response) {
+                    // resolve(response);
+                // },
+                // error(err) {
+                    // reject(err);
+                // }
+            // });
+        // });
+        // return promise;
+    // },
+    // putJSON(url, body, options = {}) {
+    	// let headers = options.headers || {};
+        // let promise = new Promise((resolve, reject) => {
+            // $.ajax({
+                // url,
+                // method: 'PUT',
+                // contentType: 'application/json',
+                // data: JSON.stringify(body),
+                // headers,
+                // success(data) {
+                    // resolve(data);
+                // },
+                // error(err) {
+                    // reject(JSON.parse(err.responseText));
+                // }
+            // });
+        // });
+        // return promise;
+    // },
+    // postJSON(url, body, options = {}) {
+    	// let headers = options.headers || {};
+    	// let promise = new Promise((resolve, reject) => {
+    		// $.ajax({
+    			// url,
+    			// method: 'POST',
+    			// contentType: 'application/json',
+    			// data: JSON.stringify(body),
+    			// headers,
+                // success(data) {
+                    // resolve(data);
+                // },
+                // error(err) {
+                    // reject(JSON.parse(err.responseText));
+                // }
+    		// });
+    	// });
+    	// return promise;
+    // },
+    // getJSON(url, options = {}) {
+    	// let headers = options.headers || {};
+    	// let promise = new Promise((resolve, reject) => {
+    		// $.ajax({
+    			// url,
+    			// method: 'GET',
+    			// headers,
+                // success(response) {
+                    // resolve(response);
+                // },
+                // error(err) {
+                    // reject(JSON.parse(err.responseText));
+                // }
+    		// });
+    	// });
+// 
+        // return promise;
+    // }
+// 
+// };
+// 
+// export { requester };
+// 
