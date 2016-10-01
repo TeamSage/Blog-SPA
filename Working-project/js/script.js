ad23deb8c391985a057fb7aa0ee5c4ecfe81a49d
Spasimira Hadzhieva <spasimira.d.hadzhieva@gmail.com>
2016-10-02 01:46:27 +0300

    nav-all links in the right corner and finalized home-page

--- Working-project/js/script.js
+++ Working-project/js/script.js
@@ -1,21 +1,35 @@
-/*(function() {
+(function() {
     'use strict';
 
     $(document).ready(function() {
-        setActiveClass();
+        setDisabledClass();
     });
 
-    function setActiveClass() {
-        $('.navbar-nav.navbar-right li').each(function() {
-            var item = $(this);
-            item.on('click', function() {
-                var item = $(this);
-                item.addClass('active');
-                
+    function setDisabledClass() {
+        const addPostButton = $('.btn-add-post');
+        const allPostsButton = $('.btn-view-post');
+        const userPostsButton = $('.btn-my-post');
+        const message = "You must be logged in";
+
+        if (true) { // isLoggedIn
+            addPostButton.on('mouseover', function() {
+                addPostButton.addClass('disabled');
+                addPostButton.html(message);
+                addPostButton.css({ "color": "#f44242" });
+            });
+            allPostsButton.on('mouseover', function() {
+                allPostsButton.addClass('disabled');
+                allPostsButton.html(message);
+                allPostsButton.css({ "color": "#f44242" });
 
             });
-});
+            userPostsButton.on('mouseover', function() {
+                userPostsButton.addClass('disabled');
+                userPostsButton.html(message);
+                userPostsButton.css({ "color": "#f44242" });
+
+            });
+        }
     }
 
 })();
-*/
