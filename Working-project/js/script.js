357d33bced944d40745a8b48fb04e170add5e779
Spasimira Hadzhieva <spasimira.d.hadzhieva@gmail.com>
2016-10-02 13:10:56 +0300

    posts without comments,new templates for initil-posts and current-post

--- Working-project/js/script.js
+++ Working-project/js/script.js
@@ -8,7 +8,7 @@
 
     function setDisabledClass() {
         const addPostButton = $('.btn-add-post');
-        const allPostsButton = $('.btn-view-post');
+        const categoriesButton = $('.btn-categories');
         const userPostsButton = $('.btn-my-post');
         const message = "You must be logged in";
 
@@ -18,10 +18,10 @@
                 addPostButton.html(message);
                 addPostButton.css({ "color": "#f44242" });
             });
-            allPostsButton.on('mouseover', function() {
-                allPostsButton.addClass('disabled');
-                allPostsButton.html(message);
-                allPostsButton.css({ "color": "#f44242" });
+            categoriesButton.on('mouseover', function() {
+                categoriesButton.addClass('disabled');
+                categoriesButton.html(message);
+                categoriesButton.css({ "color": "#f44242" });
 
             });
             userPostsButton.on('mouseover', function() {
