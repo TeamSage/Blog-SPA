2ae2c3ee4cad2844741eb3fdb9ecde192f68fdf8
Todor Georgiev <todorgeorgiev96@gmail.com>
2016-10-03 02:50:07 +0300

    systemJs to load scripts

---
+++ Working-project/tests/systemjs-config.js
@@ -0,0 +1,34 @@
+SystemJS.config({
+    transpiler: 'plugin-babel',
+
+    map: {
+        'plugin-babel': './../node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': './../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+        
+        // libs
+        'navigo': '..../bower_components/navigo/lib/navigo.js',
+        'jquery': './../bower_components/jquery/dist/jquery.js',
+        'handlebars': './../bower_components/handlebars/handlebars.js',
+        'toastr': './../bower_components/toastr/toastr.js',
+        
+        // scripts
+        'test': './test.js',
+        'app': './../scripts/app.js',
+        'data': './../scripts/data.js',
+        'user-data': './../scripts/user-data.js',
+        
+        // utils
+        'validator': './../scripts/validator.js',
+        'notifier': './../scripts/notifier.js',
+        'cleaner': './../scripts/cleaner.js',
+        'kinvey-constants': './../scripts/constants/kinvey-constants.js',
+        'requester': './../scripts/requester.js',
+        'templates-loader': './../scripts/template-loader.js',
+        
+        // controllers
+        'controller': './../scripts/controllers/controller.js',
+        'user-controller': './../scripts/controllers/user-controller.js'
+    }
+});
+
+System.import('test');
